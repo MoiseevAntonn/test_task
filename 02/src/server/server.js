ef93ce44ca405eb4f06a7094910926a06c22811f
Anton Moiseev <tony.moiseev2012@yandex.ru>
2019-09-26 18:55:39 +0300

    second task

--- 02/src/server/server.js
+++ 02/src/server/server.js
@@ -1,8 +1,119 @@
 const express = require("express");
+const db = require("./utils/DataBaseUtils");
+const bodyParser = require("body-parser");
+const path = require("path");
+const passport = require("passport");
+
 const app = express();
 
-app.get("/",async function(req,res){
-  res.redirect("https://www.google.ru");
+var sessions = [];
+
+app.use( bodyParser.json() );
+app.use( express.static(path.resolve(__dirname,"../public")) );
+
+
+app.get(/\short.*$/,async function(req,res){
+  const cookie = request.getHeader("Cookie");
+  const key = cookie.split("=")[1];
+
+  var userSession = sessions.find(session => session.key = key);
+
+  if (!userSession){
+    res.status(401).send();
+    return;
+  };
+
+  var linkMap = await db.getLink(userSession.id);
+
+  res.redirect(linkMap.shortLink);
+})
+
+app.post("/register",async function(req,res){
+  const user = req.body.user;
+  try {
+    await db.createUser(user);
+  } catch (e) {
+    res.status(500).send();
+    return;
+  };
+
+  res.redirect("/login");
+
 });
 
+app.post("/login",async function(req,res){
+  const {name,password} = req.body.user;
+  if (!name || !password){
+    res.status(400).send();
+    return;
+  };
+
+  try {
+    var user = db.getUserByName(name)
+  } catch (e) {
+    res.status(500).send();
+    return ;
+  }
+
+  if (password !== user.password){
+    res.status().send("wrong password");
+    return;
+  };
+
+
+  if (sessions.find(session => session.name = name) === undefined){
+    var sessionKey = generateSessionKey();
+    session.push({
+      id : user.id,
+      key : sessionKey
+    });
+
+    res.setHeader("Set-Cookie",[`key=${sessionKey}`]);
+    res.status(200).send();
+  }
+
+});
+
+app.post("/logout",async function(req,res){
+  const cookie = request.getHeader("Cookie");
+  const key = cookie.split("=")[1];
+
+  var userSessionIndex = sessions.findIndex(session => session.key = key);
+
+  if (!userSessionIndex === -1){
+    res.status(200).send();
+    return;
+  };
+
+  sessions.splice(userSessionIndex,1);
+  res.setHeader("Set-Cookie",[`key=''`]);
+  res.status(200).send();
+});
+
+app.post("/createLink",async function(req,res){
+  const {longLink} = req.body;
+  const shortLink = generateShortLink();
+  const linkMap = {
+    longLink,
+    shortLink,
+    value : 0
+  };
+  try {
+    await db.createLink(linkMap);
+  } catch (e) {
+    res.status(500).send();
+    return
+  }
+  res.send(linkMap);
+})
+
 app.listen(3000);
+
+
+function generateShortLink(){
+  return "localhost:3000/short" + Math.random().toString(36).substring(2, 15);
+};
+
+function generateSessionKey(){
+  return Math.random().toString(36).substring(2, 15);
+}
---
+++ 02/src/server/utils/DataBaseUtils.js
@@ -0,0 +1,49 @@
+const { Pool } = require("pg");
+
+const pool = new Pool();
+
+pool.on('error',(err,cilent) => {
+
+})
+
+
+
+
+module.exports.getUserByName = async function(name){
+  var result = await pool.query("SELECT * FROM users WHERE name = $1",[name]);
+  var user = null;
+  if (result.rowCount > 0){
+    user = result.row[0];
+  }
+  return user;
+};
+
+module.exports.getUserById = async function(id){
+  var result = await pool.query("SELECT * FROM users WHERE id = $1",[id]);
+  var user = null;
+  if (result.rowCount > 0){
+    user = result.row[0];
+  }
+  return user;
+};
+
+module.exports.createUser = async function(user){
+  await pool.query("INSERT INTO users VALUES ( $1, $2)",[user.name,user.password]);
+};
+
+module.exports.createLink = async function(limkMap){
+  await pool.query("INSERT INTO links VALUES ( $1, $2 ,$3 ,$4 )",[linkMap.id,linkMap.shortLink,linkMap.longLink,linkMap.count])
+};
+
+module.exports.getLink = async function(id){
+  var result = pool.query("SELECT * FROM links WHERE id = $1",[id]);
+  var link = null;
+  if (result.rowCount > 0){
+    link = result.row[0];
+  }
+  return link;
+};
+
+module.exports.incrementLink = async function(){
+
+};
